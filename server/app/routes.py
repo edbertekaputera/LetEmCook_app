6d71e15c95b59eaafd3d186feff643a445f21d14
edkesuma <edrickkesuma21@gmail.com>
2023-08-25 17:01:15 +0800

    added getRecipes route

--- server/app/routes.py
+++ server/app/routes.py
@@ -1,5 +1,14 @@
 from app import app
+from flask import request
+from app.createRecipes import createRecipes
 
 @app.route("/api/test")
 def test():
     return {"status": "success"}
+
+# sample request: http://localhost:5000/api/getRecipe?ingredients="onion, rice, beef, egg, chili"
+@app.route("/api/getRecipe", methods=["GET"])
+def getRecipe():
+    ingredients = request.args.get("ingredients")
+    recipe = createRecipes(ingredients)
+    return recipe
