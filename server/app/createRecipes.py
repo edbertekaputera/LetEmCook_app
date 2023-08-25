44116b1dc1764e6e310419bd78908cc3ec6d89c9
edbertekaputera <edbertekaputera@gmail.com>
2023-08-25 22:07:37 +0800

    fixed llama init

--- server/app/createRecipes.py
+++ server/app/createRecipes.py
@@ -1,45 +1,11 @@
-from langchain.llms import LlamaCpp
-from langchain import PromptTemplate, LLMChain
+from app import llama_model
 import time
 import re
 
 # expects ingredients list as one string
 def createRecipes(ingredients):
-	B_INST, E_INST = "[INST]", "[/INST]"
-	B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
-
-	CUSTOM_SYSTEM_PROMPT="You are now a professional chef that has expertise in recipe generations"
-	RULES = "Make sure you show the entire procedure for cooking it. Make sure that any extra ingredients you include are commonly found at home. Remember to include the Dish Name header."
-	FORMATTING = "The structure of the recipe shall be split to three sections in the following format. Dish Name:\n Ingredients\n Instructions\n. Make sure the instructions appear in numbered format."
-	SYSTEM_PROMPT = B_SYS + CUSTOM_SYSTEM_PROMPT + RULES + FORMATTING + E_SYS
-	instruction = "Recommend me three different easy to cook homemade recipes with the following ingredients:\n'{ingredients}'"
-
-	template = B_INST + SYSTEM_PROMPT + instruction + E_INST
-
-	# template for an instruction with input
-	prompt_with_context = PromptTemplate(
-		input_variables=["ingredients"],
-		template= template)
-
-	model_path = './app/LLM/ggml-model-q4_0.bin'
-	llm = LlamaCpp(
-		model_path=model_path,
-		n_gpu_layers=10,
-		n_batch=2048,
-		temperature=0.72,
-		n_ctx=2048,
-		max_tokens=2048,
-		top_p=0.74,
-		top_k=0,
-		f16_kv=True,
-		verbose=True,
-	) # type: ignore
-
-
-	llm_context_chain = LLMChain(llm=llm, prompt=prompt_with_context)
-
 	startTime = time.time()
-	recipes = llm_context_chain.predict(ingredients = ingredients)
+	recipes = llama_model.predict(ingredients = ingredients)
 	endTime = time.time()
 	print(f"Time for predicting: {endTime - startTime}")
     
